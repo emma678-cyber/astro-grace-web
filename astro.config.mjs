40e8355d0ef7b3c7f96a3bca0c8ae6cc1f9ec87a
emma678-cyber <emmaliu1510@gmail.com>
2026-07-13 18:23:24 -0400

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import keystatic from '@keystatic/astro';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [keystatic()]
+  integrations: [keystatic()],
+  adapter: cloudflare()
 });
